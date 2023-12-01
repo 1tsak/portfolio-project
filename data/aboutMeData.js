ce7341128bfb11ec2e72dd938fd18b8d866d0bd1
A2v10 <hey.a2v10@gmail.com>
2023-12-01 17:49:04 +0530

    Init

---
+++ data/aboutMeData.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const aboutMeData = [
+	{
+		id: uuidv4(),
+		bio: 'A full stack android and web developer with a passion for learning new technologies, i strive to use my skills make life easier for others.',
+	},
+	{
+		id: uuidv4(),
+		bio: 'Also I have intrest in Game Development, Cyber Security and Open Source',
+	},
+];
